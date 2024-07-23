057cff32e73b3de27cdf37cf03292e5e4999b6d7
vuduong97 <ddtvu@whitespace.vn>
2024-07-24 01:11:39 +0700

    Section 47: [start server] First step in Microservice vs Message Queue | Microservice System | Lvxx

--- src/tests/message-queue/rabbitmq/producer.js
+++ src/tests/message-queue/rabbitmq/producer.js
@@ -1,20 +1,20 @@
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
-const message = 'hello, RabbitMQ for Tip Javascript';
+const message = "new a product: Title abc";
 
 const runProducer = async () => {
   try {
-    const connection = await amqp.connect('amqp://guest:12345@localhost');
+    const connection = await amqp.connect("amqp://guest:12345@localhost");
     const channel = await connection.createChannel();
 
-    const queueName = 'test-topic';
+    const queueName = "test-topic";
     await channel.assertQueue(queueName, {
       durable: true,
     });
 
     // send message to consumer channel
     channel.sendToQueue(queueName, Buffer.from(message));
-    console.log('Message sent:', message);
+    console.log("Message sent:", message);
   } catch (error) {
     console.error(error);
   }
